974cea0eebc751a20eab665fce9013839dd4dfb7
Rachel Veer <rachelyoubish@gmail.com>
2021-01-15 09:03:44 -0600

    Better camera start position.

--- LearnDirectX/Camera.h
+++ LearnDirectX/Camera.h
@@ -12,9 +12,9 @@ enum CameraMovement {
 };
 
 // Default camera values.
-const float YAW = 90.0f;
+const float YAW = -90.0f;
 const float PITCH = 0.0f;
-const float SPEED = 2.5f;
+const float SPEED = 5.5f;
 
 class Camera
 {
