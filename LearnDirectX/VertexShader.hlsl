5a691980a663d8d05c1b258c293a2a5108c2c25b
Rachel Veer <rachelyoubish@gmail.com>
2021-01-20 11:29:26 -0600

    Specular lighting fixed.

--- LearnDirectX/VertexShader.hlsl
+++ LearnDirectX/VertexShader.hlsl
@@ -18,11 +18,11 @@ VSOut VSmain(float3 pos : POSITION, float3 normal : NORMAL)
     VSOut output;
     
     float4 worldPos = mul(float4(pos, 1.0f), model);
+    output.normal = float4(mul(transpose((float3x3) model), normal), 0.0f);
+    output.worldPos = worldPos;
+    
     float4x4 vp = mul(view, projection);
     float4 mvp = mul(worldPos, vp);
     output.pos = mvp;
-    
-    output.normal = float4(normal, 1.0f);
-    output.worldPos = worldPos;
     return output;
 }
