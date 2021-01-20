01a3ea84a1dc4f95cfb045d7d3e49df585b66335
Rachel Veer <rachelyoubish@gmail.com>
2021-01-20 11:22:22 -0600

    Renamed pixelPos to more proper name: world position.

--- LearnDirectX/PixelShader.hlsl
+++ LearnDirectX/PixelShader.hlsl
@@ -23,7 +23,6 @@ cbuffer Material : register(b1)
 struct Light
 {
     float4 position;
-    
     float4 ambient;
     float4 diffuse;
     float4 specular;
@@ -34,22 +33,22 @@ cbuffer Light : register(b2)
     Light light;
 }
 // Position input (even if not utilized) to match vertex shader output. 
-float4 PSmain(float4 pos : SV_POSITION, float4 color : COLOR, float4 normal : NORMAL) : SV_TARGET
+float4 PSmain(float4 pos : SV_POSITION, float4 normal : NORMAL, float4 worldPos : Position) : SV_TARGET
 {
     // Ambient 
     float4 ambient = light.ambient * material.ambient;
     
     // Diffuse
     float4 norm = normalize(normal);
-    float4 lightDir = normalize(light.position - color);
+    float4 lightDir = normalize(light.position - worldPos);
     float4 diff = max(dot(norm, lightDir), 0.0f);
     float4 diffuse = light.diffuse * (diff * material.diffuse);
     
     // Specular
-    float4 viewDir = normalize(viewPos - color);
-    float4 reflectDir = reflect(lightDir, norm);
+    float4 viewDir = normalize(viewPos - worldPos);
+    float4 reflectDir = reflect(-lightDir, norm);
     
-    float4 spec = pow(max(dot(viewDir, -reflectDir), 0.0f), material.shininess);
+    float4 spec = pow(max(dot(viewDir, reflectDir), 0.0f), material.shininess);
     float4 specular = light.specular * (spec * material.specular);
     
     float4 result = ambient + diffuse + specular;
--- LearnDirectX/VertexShader.hlsl
+++ LearnDirectX/VertexShader.hlsl
@@ -9,20 +9,20 @@ cbuffer ConstantBuffer : register(b0)
 struct VSOut
 {
     float4 pos : SV_POSITION;
-    float4 color : COLOR;
     float4 normal : NORMAL;
+    float4 worldPos : Position;
 };
 
 VSOut VSmain(float3 pos : POSITION, float3 normal : NORMAL)
 {
     VSOut output;
     
-    float4 PixelPos = mul(float4(pos, 1.0f), model);
+    float4 worldPos = mul(float4(pos, 1.0f), model);
     float4x4 vp = mul(view, projection);
-    float4 mvp = mul(PixelPos, vp);
+    float4 mvp = mul(worldPos, vp);
     output.pos = mvp;
     
     output.normal = float4(normal, 1.0f);
-    output.color = PixelPos;
+    output.worldPos = worldPos;
     return output;
 }
