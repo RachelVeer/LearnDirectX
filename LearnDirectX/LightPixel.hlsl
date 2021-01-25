1efdeffb2ecb9091777811f8b65d19788ee4a22c
Rachel Veer <rachelyoubish@gmail.com>
2021-01-25 14:17:49 -0600

    Light cube object matches light color.

--- LearnDirectX/LightPixel.hlsl
+++ LearnDirectX/LightPixel.hlsl
@@ -1,4 +1,17 @@
+struct Light
+{
+    float4 position;
+    float4 ambient;
+    float4 diffuse;
+    float4 specular;
+};
+
+cbuffer Light : register(b2)
+{
+    Light light;
+}
+
 float4 PSmain(float4 pos : SV_POSITION) : SV_TARGET
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+	return light.diffuse;
 }
